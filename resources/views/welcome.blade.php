235a12d68d7ca585325c9cc80dcd28a0b39999ee
quanle51297 <quanle51297@gmail.com>
2019-11-13 18:40:47 +0700

    sagophone project
    sagophone project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel quan le
+                    Laravel
                 </div>
 
                 <div class="links">
