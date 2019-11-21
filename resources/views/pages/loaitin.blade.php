ae59c364fb8e652fe6fdea7c2ceefb1cfdbded90
mo <huyvo7533@gmail.com>
2019-11-21 16:57:55 +0700

    tao mợt thiệt
    tao mợt thiệt

--- resources/views/pages/loaitin.blade.php
+++ resources/views/pages/loaitin.blade.php
@@ -13,8 +13,8 @@
                         <li><a href="hotdeals">Hot Deals</a></li>
                         <li class="active"><a href="loaitin">Tin Tức</a></li>
                         <li><a href="danhmuc">Danh Mục</a></li>
-                        <li><a href="smartphones">Smartphones</a></li>                                  
-                        <li><a href="phukien">Phụ Kiện</a></li>
+                        <li><a href="danhmuc/1/Điện thoại}">Điện Thoại</a></li>                                 
+                        <li><a href="danhmuc/2/Phụ kiện">Phụ Kiện</a></li>
                     </ul>
                     <!-- /NAV -->
                 </div>
