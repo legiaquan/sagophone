67feae2e1a8a99ef5f508a4888214a5393d84a1f
quanle51297 <quanle51297@gmail.com>
2019-12-05 11:49:26 +0700

    css

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.index')
 @section('css')
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="css/w3.css">
 <style>
 .mySlides {display:none}
 .w3-left, .w3-right, .w3-badge {cursor:pointer}
